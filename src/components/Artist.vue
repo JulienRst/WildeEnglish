e25ede738542b821081c233610edc415e14a88ae
julienr-geronimo <julien.r@geronimostudios.com>
2018-07-05 01:30:41 +0200

    Add Artists scroll with mouse

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -2,7 +2,7 @@
   <section class="artist">
     <h2>Parmi les artistes et producteurs <br />qui me font confiance :</h2>
     <div class="phantom-artist-container">
-      <div class="ctn-artists frow">
+      <div id="artists" class="ctn-artists frow" @mousedown="startCapture($event)" @mouseup="endCapture()" @mousemove="capture($event)">
         <div :style="{zIndex: artists.length - artist.key}" :class="{'ctn-artist fcolumn': true, 'top': (artist.key % 2 === 0), 'bottom': (artist.key % 2 !== 0)}" v-for="artist in artists" v-bind:key='artist.key'>
           <img class="artist-img" :src="generateUrl(artist.img)" alt="">
           <div class="artist-name">
@@ -21,12 +21,36 @@ export default {
   name: 'Artist',
   data() {
     return {
-      artists: Artists
+      artists: Artists,
+      captureStatus: false,
+      startCaptureValue: 0,
+      artistsRef: null,
+      basis: 150,
+      current: 0
     }
+  },
+  mounted() {
+    this.artistsRef = document.getElementById('artists')
   },
   methods: {
     generateUrl(end) {
       return `/static/img/artists/${end}`
+    },
+    startCapture(e) {
+      this.startCaptureValue = e.screenX
+      this.captureStatus = true
+    },
+    capture(e) {
+      if (this.captureStatus) {
+        const diff = e.screenX - this.startCaptureValue
+        this.current = this.basis - diff
+        this.artistsRef.style.left = `${this.basis - diff}px`
+      }
+    },
+    endCapture() {
+      this.basis = this.current
+      this.startCaptureValue = 0
+      this.captureStatus = false
     }
   }
 }
--- src/fixtures/artists.js
+++ src/fixtures/artists.js
@@ -28,6 +28,36 @@ const Artists = [
     key: 6,
     name: 'Stromae',
     img: 'artistes6.png'
+  },
+  {
+    key: 7,
+    name: 'Agnes Obel',
+    img: 'artistes1.png'
+  },
+  {
+    key: 8,
+    name: 'Benjamine Clementine',
+    img: 'artistes2.png'
+  },
+  {
+    key: 9,
+    name: 'Alfred Brendel',
+    img: 'artistes3.png'
+  },
+  {
+    key: 10,
+    name: 'Art Garkunfel',
+    img: 'artistes4.png'
+  },
+  {
+    key: 11,
+    name: 'Bruno Mars',
+    img: 'artistes5.png'
+  },
+  {
+    key: 12,
+    name: 'Stromae',
+    img: 'artistes6.png'
   }
 ]
 
